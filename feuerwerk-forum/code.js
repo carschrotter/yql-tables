0a5e462e137c288754d087cd9add09fd17629d3a
MNHcC (Michael Hegenbarth) <carschrotter@gmail.com>
2013-09-13 19:59:46 +0200

    create javascript

--- feuerwerk-forum/code.js
+++ feuerwerk-forum/code.js
@@ -1,6 +1,6 @@
-window = {};
+var window = {}, document = {};
 y.include("https://datejs.googlecode.com/files/date.js");
-y.include("https://raw.github.com/coolaj86/node-jquery/master/lib/node-jquery.js");
+y.include("https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js");
 function getFormattedDate(date) {
     var str = date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate()
     return str;
