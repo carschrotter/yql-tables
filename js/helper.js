788267d39ab4614944eeecfdf55484a4b3ef004d
MNHcC (Michael Hegenbarth) <carschrotter@gmail.com>
2013-09-13 20:31:01 +0200

    create javascript

--- js/helper.js
+++ js/helper.js
@@ -3,10 +3,17 @@
 //        callback(k, this[ k ]);
 //    }
 //}
-//Object.prototype.foreach = function(callback) {
-//    for (var k in this) {
-//        if (this.hasOwnProperty(k)) {
-//            callback(k, this[ k ]);
-//        }
-//    }
-//}
+Object.prototype.forEach = function(callback /*, thisp*/)
+{
+    if (typeof callback !== "function") {
+        throw new TypeError();
+    }
+
+    var thisp = arguments[1];
+    for (var i = 0; i < this.length; i++)
+    {
+        if (i in this) {
+            callback.call(thisp, this[i], i, this);
+        }
+    }
+};
