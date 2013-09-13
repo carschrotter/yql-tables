010dff477cc757a9a4c38f98358d7cfded875b0a
MNHcC (Michael Hegenbarth) <carschrotter@gmail.com>
2013-09-13 20:27:09 +0200

    create javascript

--- feuerwerk-forum/code.js
+++ feuerwerk-forum/code.js
@@ -1,6 +1,5 @@
-var window = {}, document = {};
+y.include("https://raw.github.com/carschrotter/yql-tables/master/js/helper.js");
 y.include("https://datejs.googlecode.com/files/date.js");
-y.include("https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js");
 function getFormattedDate(date) {
     var str = date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate()
     return str;
@@ -17,7 +16,7 @@ else {
 y.log(d);
 url = 'http://www.feuerwerk-forum.de/calendar.php?do=getinfo&day=' + getFormattedDate(d) + '&c=' + calendar;
 query = y.query("select * from html where url=@url and xpath=@xpath", {url: url, xpath: xpath});
-$.each(query.results, function(index, value) {
+query.results.forEach( function(index, value) {
     response.object[index] = <event><ort></ort><value>{value}</value></event>;
 }); 
 //;
---
+++ js/helper.js
@@ -0,0 +1,12 @@
+//Array.prototype.foreach = function(callback) {
+//    for (var k = 0; k < this.length; k++) {
+//        callback(k, this[ k ]);
+//    }
+//}
+//Object.prototype.foreach = function(callback) {
+//    for (var k in this) {
+//        if (this.hasOwnProperty(k)) {
+//            callback(k, this[ k ]);
+//        }
+//    }
+//}
