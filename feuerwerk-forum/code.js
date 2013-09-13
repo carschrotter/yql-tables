4bdf38a51ce84a8b6d98b69f4f91586648ef974f
MNHcC (Michael Hegenbarth) <carschrotter@gmail.com>
2013-09-13 19:16:42 +0200

    fix example

--- feuerwerk-forum/code.js
+++ feuerwerk-forum/code.js
@@ -1,11 +1,18 @@
+y.include("http://datejs.googlecode.com/files/date.js");
 function getFormattedDate(date) {
     var str = date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate()
     return str;
 }
 calendar = calendar || 1;
-var query = null, d = null, url = '', xpath='/html/body/div/div/div/div/form';
-if (date) { d = new Date(date); }
-else { d = new Date(); }
+var query = null, d = null, url = '', xpath = '/html/body/div/div/div/div/form';
+y.log(date);
+if (date) {
+    d = Date.parse(date);
+}
+else {
+    d = new Date();
+}
+y.log(d);
 url = 'http://www.feuerwerk-forum.de/calendar.php?do=getinfo&day=' + getFormattedDate(d) + '&c=' + calendar;
-query = y.query("select * from html where url=@url and xpath=@xpath",{url:url, xpath:xpath});
+query = y.query("select * from html where url=@url and xpath=@xpath", {url: url, xpath: xpath});
 response.object = query.results;
