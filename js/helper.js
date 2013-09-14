51e8c1192cd6f4afce1c69652659f1cc5035eac2
MNHcC (Michael Hegenbarth) <carschrotter@gmail.com>
2013-09-14 09:04:29 +0200

    edit javascript

--- js/helper.js
+++ js/helper.js
@@ -4,7 +4,7 @@ Object.prototype.forEach = function(callback /*, thisp*/)
         throw new TypeError();
     }
     var thisp = arguments[1] || this;
-    var interCallback = interCallback.bind(thisp);
+    var interCallback = callback.bind(thisp);
     for (var key in this) {
         if (key in this) {
             interCallback.call(this[key], key, this);
