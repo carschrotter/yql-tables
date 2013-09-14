cfdec4b2b171def0615a12bfccd606241bc16a52
MNHcC (Michael Hegenbarth) <carschrotter@gmail.com>
2013-09-14 09:45:16 +0200

    edit javascript

--- feuerwerk-forum/code.js
+++ feuerwerk-forum/code.js
@@ -17,7 +17,8 @@ url = 'http://www.feuerwerk-forum.de/calendar.php?do=getinfo&day=' + getFormatte
 query = y.query("select * from html where url=@url and xpath=@xpath", {url: url, xpath: xpath});
 var results = <events></events>;
 query.results.forEach(function(value, index) {
-    y.log(value, index);
+    y.log(value);
+    y.log(index);
 //    var form = value.form;
 //    form.input.forEach(function(v, i) {
 //        if(v.name ='') {};
